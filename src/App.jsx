12b1a3975b2a8958c8c75bfa220340b46b7187fe
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-08-05 16:18:44 -0700

    feat: add functionality to add tasks to selected project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,26 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
 
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
@@ -70,6 +89,9 @@ function App() {
     <SelectedProject
       project={selectedProjects}
       onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
     />
   );
 
